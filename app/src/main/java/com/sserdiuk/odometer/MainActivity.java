066d66f493c826af731abc115e4a9b443071b1a8
Serdiuk Serhii <serduksergey90@gmail.com>
2018-02-23 00:38:43 +0200

    connect OdometerService to MainActivity

--- app/src/main/java/com/sserdiuk/odometer/MainActivity.java
+++ app/src/main/java/com/sserdiuk/odometer/MainActivity.java
@@ -1,9 +1,33 @@
 package com.sserdiuk.odometer;
 
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private OdometerService odometerService;
+    private boolean bound = false;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            OdometerService.OdometerBinder odometerBinder = (OdometerService.OdometerBinder) iBinder;
+
+//            Преобразовать Binder
+//            в OdometerBinder, после чего использовать для получения ссылки на OdometerService.
+            odometerService = odometerBinder.getOdometer();
+//            Если активность связана со службой, bound присваивается true.
+            bound = true;
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            bound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
